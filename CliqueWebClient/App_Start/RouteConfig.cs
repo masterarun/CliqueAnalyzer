68f9f79439f6cd2cb047dd15d9231713cd1ceb4b
seshv4u <seshu.venna@gmail.com>
2015-09-10 08:39:21 +0530

    Changing the order of menu and chaged the default view in RouteConfig

--- CliqueWebClient/App_Start/RouteConfig.cs
+++ CliqueWebClient/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CliqueWebClient
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Location", id = UrlParameter.Optional }
             );
 
         }

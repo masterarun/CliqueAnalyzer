e3bb526f1cf97508fa07ff949a2c92e55aa88394
seshv4u <seshu.venna@gmail.com>
2015-09-12 18:02:41 +0530

    Added LocationRequestModel. But the modal for adding location details is not launching. will look into it.

--- CliqueWebClient/Scripts/Home/DashboardViewModel.js
+++ CliqueWebClient/Scripts/Home/DashboardViewModel.js
@@ -22,32 +22,27 @@ var DashboardViewModel = function () {
     var self = this;
 
     self.hashTagRequestList = ko.observableArray([]);
-
+    self.locationRequestList = ko.observableArray([]);
     self.displayAddHashTagModal = ko.observable(false);
-
+    self.displayAddLocationModal = ko.observable(false);
     self.hashTagRequestModel = ko.observable(new HashTagRequestModel());
-
+    self.locationRequestModel = ko.observable(new LocationRequestModel());
     self.showTagRequestModal = function (item) {
         debugger;
-        
         self.displayAddHashTagModal(true);
-        
-
     };
-
     self.hideTagRequestModal = function () {
         self.displayAddHashTagModal(false);
-        
-
     };
-
-
-
+    self.showLocationRequestModal = function (item) {
+        debugger;
+        self.displayAddLocationModal(true);
+    };
+    self.hideLocationRequestModal = function () {
+        self.displayAddLocationModal(false);
+    };
     self.getHashTagRequest = function () {
         debugger;
-
-
-
         $.ajax({
             type: "Get",
             contentType: "application/json",
@@ -60,20 +55,10 @@ var DashboardViewModel = function () {
             debugger;
             alert("Error");
         });
-
-
-
     };
-
-
-
     debugger;
-
     self.getHashTagRequest();
-
     debugger;
-
-
     self.addHashTagRequest = function () {
         debugger;
         var item = ko.toJSON(self.hashTagRequestModel());
@@ -93,7 +78,45 @@ var DashboardViewModel = function () {
             alert("Error");
         });
     };
-    
+
+    self.getLocationRequest = function () {
+        debugger;
+        $.ajax({
+            type: "Get",
+            contentType: "application/json",
+            url: baseUrl + "api/CliqueAPI/GetAllLocationRequest"
+        }).done(function (res) {
+            debugger;
+            self.locationRequestList(res);
+            $("#locationTable").DataTable({ responsive: true });
+        }).error(function (ex) {
+            debugger;
+            alert("Error");
+        });
+    };
+
+    self.getLocationRequest();
+
+    self.addLocationRequest = function () {
+        debugger;
+        var item = ko.toJSON(self.locationRequestModel());
+        debugger;
+        $.ajax({
+            type: "POST",
+            contentType: "application/json",
+            data: item,
+            url: baseUrl + "api/CliqueAPI/AddLocationRequest"
+        }).done(function (res) {
+            debugger;
+            self.hideLocationRequestModal();
+            self.getLocationRequest();
+
+        }).error(function (ex) {
+            debugger;
+            alert("Error");
+        });
+    };
+
 };
 
 
---
+++ CliqueWebClient/Scripts/Home/LocationRequestModel.js
@@ -0,0 +1,45 @@
+﻿function LocationRequestModel() {
+    debugger;
+    var self = this;
+
+    self.address = ko.observable();
+    self.street = ko.observable();
+    self.city = ko.observable();
+    self.state = ko.observable();
+    self.id = ko.observable();
+
+    self.postalCode = ko.observable();
+
+    self.latitude = ko.observable();
+    self.longitude = ko.observable();
+
+    self.postalCode.subscribe(function () {
+        debugger;
+        if (self.postalCode().trim() == "") {
+            self.latitude("");
+            self.longitude("");
+            return;
+        }
+        self.GetGeoLocation();
+    });
+
+    self.GetGeoLocation = function () {
+        debugger;
+        var geocoder = new google.maps.Geocoder();
+        var address = self.postalCode().trim();
+
+        geocoder.geocode({ 'address': address }, function (results, status) {
+            debugger;
+            if (status == google.maps.GeocoderStatus.OK) {
+                self.latitude(results[0].geometry.location.lat());
+                self.longitude(results[0].geometry.location.lng());
+            } else {
+                alert("Geolocation api failed.");
+            }
+        });
+    };
+
+
+
+
+}
